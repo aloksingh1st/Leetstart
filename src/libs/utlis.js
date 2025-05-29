7cf64daa358ad98458ff486ad08d78775022eee9
aloksingh1st <aloksingh1st@gmail.com>
2025-05-29 21:56:42 +0530

    langeuage id comment

--- src/libs/utlis.js
+++ src/libs/utlis.js
@@ -10,13 +10,14 @@ function getLanguageName(languageId) {
 
 export { getLanguageName };
 
-
+// all language and their respective judge0 ids
 export function getLanguageId(language) {
     const languageMap = {
         "PYTHON": 71,
         "JAVASCRIPT": 63,
         "JAVA": 62,
         "TypeScript": 74,
+
     };
     return languageMap[language.toUpperCase()];
 }
