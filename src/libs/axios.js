8478e7d6a0c54fab59a7a30134185ce026be16fd
aloksingh1st <aloksingh1st@gmail.com>
2025-06-04 09:40:30 +0530

    revert

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,8 +1,6 @@
-import { meta } from "@eslint/js";
 import axios from "axios";
 
-console.log(import.meta.env.VITE_API_URL);
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1" : import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1" : "https://leetstart.onrender.com/api/v1",
     withCredentials: true,
 });
