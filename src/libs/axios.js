7bc5de0ee9e840ce8f2023e9f903df4b9d91fed3
aloksingh1st <aloksingh1st@gmail.com>
2025-06-04 09:40:37 +0530

    revert

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// console.log(import.meta.env.VITE_API_URL);
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1" : "https://leetstart.onrender.com/api/v1",
     withCredentials: true,
