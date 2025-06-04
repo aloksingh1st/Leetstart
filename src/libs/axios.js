a27f28a30478620ae6704ff1086a7d2989e1ccb7
aloksingh1st <aloksingh1st@gmail.com>
2025-06-04 09:30:06 +0530

    production backed uri

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,6 +1,8 @@
+import { meta } from "@eslint/js";
 import axios from "axios";
 
+console.log(import.meta.env.VITE_API_URL);
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1" : "https://leetstart.onrender.com/api/v1",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1" : import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
