23cde0952ce5212254c39dd2a224e4f5e671423f
aloksingh1st <aloksingh1st@gmail.com>
2025-06-12 19:27:22 +0530

    moving from render

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// console.log(import.meta.env.VITE_API_URL);
+console.log(import.meta.env.VITE_API_URL);
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1" : "https://leetstart.onrender.com/api/v1",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1" : import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
